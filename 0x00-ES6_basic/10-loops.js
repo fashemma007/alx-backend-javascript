6e048c69fa0f9d8c80e53dcb0ba32196c93efe7c
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-17 23:18:14 +0100

    feat[task 10]: appending to an array

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue (array, appendString) {
+  // console.log(`This is the array ${array}`);
+  const result = [];
+  for (const idx of array) {
+    // console.log(`IDX is ${idx}`);
+    result.push(appendString + idx);
+  }
+
+  return result;
+}
