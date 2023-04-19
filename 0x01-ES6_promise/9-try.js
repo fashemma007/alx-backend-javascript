13080c39ab84ed407b838bd60090fa4edc575f66
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-19 22:39:15 +0100

    fix:

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,8 +1,10 @@
-export default function guardrail(mathFunc) {
+export default function guardrail(mathFunction) {
   const queue = [];
-  try { //append func result to queue
-    queue.push(mathFunc(), 'Guardrail was processed');
-  } catch (error) { //catch and append error msg to queue
+  try {
+    //append func result to queue
+    queue.push(mathFunction(), 'Guardrail was processed');
+  } catch (error) {
+    //catch and append error msg to queue
     queue.push(`Error: ${error.message}`, 'Guardrail was processed');
   }
   return queue;
