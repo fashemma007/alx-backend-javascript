b0d7ed47c31928f0622cdf080ce5fea88c974268
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-19 16:45:59 +0100

    feat:

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,26 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
 }
+
+// export default function handleProfileSignup(firstName, lastName, fileName) {
+//   const signup = signUpUser(firstName, lastName);
+//   const photoUplaod = uploadPhoto(fileName);
+
+//   return Promise.allSettled([signup, photoUplaod])
+//     .then((data) => {
+//       const result = [];
+//       data.forEach((valz) => {
+//         if (valz.status === 'fulfilled') {
+//           result.push({ status: valz.status, value: valz.value });
+//         } else {
+//           result.push({
+//             status: valz.status,
+//             value: `Error: ${valz.reason.message}`,
+//           });
+//         }
+//       });
+//     return result;
+//   });
+// }
