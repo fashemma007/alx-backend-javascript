cc7f58f51617a7266bb3f85549bdd671859ea889
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-20 21:28:18 +0100

    feat[task-0]: create class ClassRoom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+// create a ClassRoom class
+export default class ClassRoom {
+  // instantiate with a attribute `maxStudentsSize`
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
