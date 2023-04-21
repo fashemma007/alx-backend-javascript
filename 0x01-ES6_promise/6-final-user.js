66fcc067361745a306fb65cb0c5f01d524755e34
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-21 14:46:01 +0100

    fix[lint]: fix linter bugs

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 // export default function handleProfileSignup(firstName, lastName, fileName) {
 //   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
@@ -22,6 +22,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
           });
         }
       });
-    return result;
-  });
+      return result;
+    });
 }
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,10 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    //append func result to queue
+    // append func result to queue
     queue.push(mathFunction(), 'Guardrail was processed');
   } catch (error) {
-    //catch and append error msg to queue
+    // catch and append error msg to queue
     queue.push(`Error: ${error.message}`, 'Guardrail was processed');
   }
   return queue;
