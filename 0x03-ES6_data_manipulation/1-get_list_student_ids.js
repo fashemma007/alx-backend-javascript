4610e4ca6243afb108c7e16439f79dfb3fcee89e
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-24 17:52:47 +0100

    fix: use arrow funcs instead

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,18 +1,18 @@
 /**
+   * getIds
+   * @param {*} objects
+   * @returns the id of a given object
+   */
+const getIds = (obj) => obj.id;
+
+/**
  *
  * @param {*} array
  * @returns an array of ids
  */
 export default function getListStudentIds(array) {
   let newArr = [];
-  /**
-   * getIds
-   * @param {*} objects
-   * @returns the id of a given object
-   */
-  function getIds(obj) {
-    return obj.id;
-  }
+
   if (Array.isArray(array)) { // check if param is an array
     // use `map` to call back `getIds` for each elemet in array
     newArr = array.map(getIds);
