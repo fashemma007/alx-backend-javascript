dfac765a758ec66ca2f2a22a88968c227f34b178
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-10 07:39:15 +0000

    [CHORE] Using Process STDIN
    Created a func taht takes user input and writes to console

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,20 @@
+const process = require('process');
+process.stdin.setEncoding('utf8');
+
+const welcome = "Welcome to Holberton School, what is your name?";
+console.log(welcome);
+
+process.stdin.on('readable', function () {  // allow
+  // The user should be able to input their name on a new line
+  var name = process.stdin.read();
+  if (name !== null) {
+    // The program should display Your name is: INPUT
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+// When the user ends the program
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
+process.stdin.read;
