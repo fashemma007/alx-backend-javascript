c2b1bde3379639cd941f136a64e1a219f0544334
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-10 06:55:13 +0000

    [CHORE] Basic JS in NodeJS
    Created a func that prints an argument to STDOUT

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,9 @@
-#!/usr/bin/node
-
-
+/**
+ * Prints in STDOUT the string `args`
+ * @param {String} args
+ */
 function displayMessage(args) {
-  console.log(args)
+  console.log(args);
 }
 
 module.exports = displayMessage;
