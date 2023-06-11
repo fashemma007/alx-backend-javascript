1aed9e853f1ef45f0e124d8af16a87a8d432ab8b
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-11 08:39:43 +0000

    CHORE

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,8 @@
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 /*
  * No need to import or `require` process
  * process.stdin.isTTY; returns True if user typed in console
  * & False if variable is passed to the program using redirections`> | ...`
- */
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on('readable', function () {  // allow
   // The user should be able to input their name on a new line
@@ -17,3 +16,18 @@ process.stdin.on('readable', function () {  // allow
 process.stdin.on('end', () => {
   process.stdout.write("This important software is now closing\n");
 });
+*/
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
