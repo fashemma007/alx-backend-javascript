d3149efe39fd1c5620301ea3ab519f7aad63b5ef
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-11 09:10:07 +0000

    FIX

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,16 @@
-/*
- * No need to import or `require` process
- * process.stdin.isTTY; returns True if user typed in console
- * & False if variable is passed to the program using redirections`> | ...`
- */
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.on('readable', function () {  // allow
-  // The user should be able to input their name on a new line
-  let name = process.stdin.read();
-  if (name !== null) {
-    // The program should display Your name is: INPUT
-    process.stdout.write(`Your name is: ${name}`);
-  }
-});
-// When the user ends the program
-process.stdin.on('end', () => {
-  process.stdout.write("This important software is now closing\n");
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
