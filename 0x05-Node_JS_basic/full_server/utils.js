91392619aac298e5a44343aabae9cdbf6ec53904
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-12 12:21:59 +0100

    [CHORE] Utility function
    Created readDatabase that accepts a file path as argument and return an object of arrays of the firstname of students per fields

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,48 @@
+import readFile from 'fs';
+
+export default function readDatabase(filePath) {
+  const students = {}; // initialize empty dict
+  const fields = {}; // initialize empty dict
+  let length = 0; // length counter
+  return new Promise((resolve, reject) => {
+    readFile(filePath, (error, data) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      } else {
+        let results = '';
+        const lines = data.toString().split('\n');
+        // console.log(lines);
+        // lines = lines.slice(1); // delete the csv headers
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            const field = lines[i].toString().split(',');
+            if (field[0] === '') { // chk for empty lines
+              // console.log("All good");
+              length += 0;
+            } else {
+              // console.log(field[0]);
+              length += 1;
+              if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+                students[field[3]].push(field[0]);
+              } else {
+                students[field[3]] = [field[0]];
+              }
+              if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+                fields[field[3]] += 1;
+              } else {
+                fields[field[3]] = 1;
+              }
+            }
+          }
+        }
+        results += `Number of students: ${length - 1}\n`; // account for headers
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            results += (`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}\n`);
+          }
+        }
+        resolve(results);
+      }
+    });
+  });
+}
