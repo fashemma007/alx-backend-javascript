eccd4bedf1414186161305da5b6d799b4a9e5120
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-16 20:42:11 +0100

    [REFACTOR] Stub variable
    Assign stubbed func to a variable

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -8,19 +8,19 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
+  let UtilCalculateNumber;
   before(() => {
     // wrap method calculateNumber of class Utils as stub method
-    sinon
-      .stub(Utils, 'calculateNumber')
+    UtilCalculateNumber = sinon.stub(Utils, 'calculateNumber')
       .returns(10);
   });
   it('verify console.log', () => {
     const consoleLogSpy = sinon.spy(console, 'log');
     sendPaymentRequestToApi(100, 20);
 
-    expect(Utils.calculateNumber.called).to.be.true;
+    expect(UtilCalculateNumber.called).to.be.true;
     expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
-    Utils.calculateNumber.restore();
+    UtilCalculateNumber.restore();
     consoleLogSpy.restore();
   });
 });
