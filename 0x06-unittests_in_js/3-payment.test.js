384bac96dd42630ce8560298078ae9e7c23d68e1
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-06-16 04:58:33 +0100

    [TEST] Sinon Stubs
    Wrap methods and funcions to speed up tests

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-hooks */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable jest/valid-expect */
 /* eslint-disable no-unused-expressions */
@@ -8,29 +9,26 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('uses the calculateNumber method of Utils', () => {
-    // now spy on the method calculateNumber of class Utils
+  beforeEach(() => {
     sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(100, 20);
-
-    expect(Utils.calculateNumber.called).to.be.true;
+  });
+  afterEach(() => {
     Utils.calculateNumber.restore();
   });
-  it('arguments passed', () => {
+  it('uses the calculateNumber method of Utils', () => {
     // now spy on the method calculateNumber of class Utils
-    sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(Utils.calculateNumber.called).to.be.true;
+  });
+  it('arguments passed', () => {
     sendPaymentRequestToApi(100, 20);
     expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
     expect(Utils.calculateNumber.callCount).to.be.equal(1);
-    Utils.calculateNumber.restore();
   });
 
   it('multiple calls', () => {
-    // now spy on the method calculateNumber of class Utils
-    sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
     sendPaymentRequestToApi(10, 25);
     expect(Utils.calculateNumber.callCount).to.be.equal(2);
-    Utils.calculateNumber.restore();
   });
 });
---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable jest/prefer-expect-assertions */
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  before(() => {
+    // wrap method calculateNumber of class Utils as stub method
+    sinon
+      .stub(Utils, 'calculateNumber')
+      .returns(10);
+  });
+  it('verify console.log', () => {
+    const consoleLogSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+
+    expect(Utils.calculateNumber.called).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+    Utils.calculateNumber.restore();
+  });
+});
